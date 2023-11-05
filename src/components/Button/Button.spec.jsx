0ac17a35b1c82032db691bacd9d357d5f96c566a
Matheus <matheusademir@hotmail.com>
2023-11-05 14:49:27 -0300

    implementacao de testes - introducao

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { Button } from "."
+import userEvent from "@testing-library/user-event";
+
+describe('<Button />',() =>{
+    it('should render the button with the text "Load More"', () =>{
+        render(<Button text="Load More" />);
+        expect.assertions(1);
+        const button = screen.getByRole('button', {name: /load more/i });
+        expect(button).toBeInTheDocument();
+    });
+
+    it('should call function on button click', () =>{
+        const fn = jest.fn();
+        render(<Button text="Load More" onClick={fn} />);
+        const button = screen.getByRole('button', {name: /load more/i });
+        userEvent.click(button);
+        userEvent.click(button);
+        expect(fn).toHaveBeenCalledTimes(2);
+        
+    });
+    
+
+    it('should be disabled when disabled is true', () =>{
+        render(<Button text="Load More" disabled={false} />);
+        const button = screen.getByRole('button', {name: /load more/i });
+        expect(button).toBeEnabled();
+        
+    });
+})
